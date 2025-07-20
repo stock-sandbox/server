f5ac65b76ca3adc07f673d5aca24050f8113847b
seokmin <msm0748@gmail.com>
2025-07-20 23:18:35 +0900

    refactor: kakao oauth 타입 분리

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,32 +8,7 @@ import { ConfigService } from '@nestjs/config';
 import { PrismaService } from '../prisma/prisma.service';
 import { JwtAuthService, JwtPayload } from './jwt.service';
 import { User } from 'generated/prisma';
-
-interface KakaoTokenResponse {
-  access_token: string;
-  token_type: string;
-  refresh_token: string;
-  expires_in: number;
-  scope: string;
-  refresh_token_expires_in: number;
-}
-
-interface KakaoUserInfo {
-  id: number;
-  connected_at: string;
-  kakao_account: {
-    profile_nickname_needs_agreement: boolean;
-    profile: {
-      nickname: string;
-      is_default_nickname: boolean;
-    };
-    has_email: boolean;
-    email_needs_agreement: boolean;
-    is_email_valid: boolean;
-    is_email_verified: boolean;
-    email: string;
-  };
-}
+import { KakaoTokenResponse, KakaoUserInfo } from './types/kakao.types';
 
 @Injectable()
 export class AuthService {
